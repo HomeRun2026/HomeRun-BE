209bfb5096a54750cd2c6eb36967f9a1ba4f8c6b
he-on <hyeonjub2911@g.hongik.ac.kr>
2026-03-26 22:49:39 +0900

    feat: 초기 프로젝트 구조 생성

---
+++ src/test/java/com/HomeRun/HomeRunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HomeRun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
